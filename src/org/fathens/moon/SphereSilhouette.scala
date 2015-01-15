d34c7e576984fc18d0508c31998363890a0df2b6
Kunio Sawatani <devel@fathens.org>
2015-01-15 17:57:14 +0900

    半周したら反対側から欠けていくように修正

--- src/org/fathens/moon/SphereSilhouette.scala
+++ src/org/fathens/moon/SphereSilhouette.scala
@@ -3,15 +3,16 @@ package org.fathens.moon
 import math._
 
 object SphereSilhouette {
-  def toRadians(denominator: Int)(numerator: Int): Double = {
-    Pi * 2 * numerator / denominator
-  }
   /**
    * true: Light
    * false: Dark
    * None: Out of Circle
    */
-  def on(r: Double)(radians: Double)(x: Double, y: Double): Option[Boolean] = {
+  def on(r: Double)(denominator: Int)(numerator: Int)(x: Double, y: Double): Option[Boolean] = {
+    val radians = {
+      val a = Pi * 2 * numerator / denominator
+      if (a < Pi) a else a + Pi
+    }
     if (y < -r || r < y) None else {
       val w = sqrt(pow(r, 2) - pow(y, 2))
       val b = w * cos(radians)
