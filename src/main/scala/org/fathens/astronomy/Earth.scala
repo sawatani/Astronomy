723fc4afed2dbd12e0f4f3feef4dd9bb4900d0e0
Kunio Sawatani <devel@fathens.org>
2015-01-27 13:11:34 +0900

    Introduce Earth's radius

--- src/main/scala/org/fathens/astronomy/Earth.scala
+++ src/main/scala/org/fathens/astronomy/Earth.scala
@@ -3,5 +3,6 @@ package org.fathens.astronomy
 import org.fathens.math._
 
 object Earth {
-  val radius = Killometers(6378.16)
+  val equatorial_radius = Killometers(6378.16)
+  val polar_radius = Killometers(6356.8)
 }
